1b923e7715a39809d7506d7bf8c85a3d69207fd0
Nelly Chepkemoi <nellychepkem2015@gmail.com>
2023-11-06 23:50:06 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import NavBar from "./components/NavBar";
 import { Route, Routes } from "react-router";
 import Home from "./components/Home";
 import Destination from "./components/Destination";
+
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/destination" element={<Destination />} />
+      <Route path="/destination" element={<Destination />}
+       />
     </Routes>
     </div>
   );
