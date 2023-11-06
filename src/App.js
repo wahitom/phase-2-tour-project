d36f11857c8386a54693c97973e37793ccc69fc1
Nelly Chepkemoi <nellychepkem2015@gmail.com>
2023-11-07 01:02:55 +0300

    footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import Footer from "./components/Footer";
 
 
 function App() {
   return (
     <div className="App">
-      
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,54 @@
+import { CardFooter } from "react-bootstrap"
+
+function Footer() {
+  return (
+    <div className='main-footer' >
+        <div className='container'>
+            <div className='row'>
+                <div className='col'>
+                    <h4>HOLIDAY TOUR & TRAVELS</h4>
+                    <ul>
+                        <li>Kenya's leading tour and travels</li>
+                        <li>Booking website,Over 10,000 packages ountry wide</li>
+                        <li>Do not miss out on offers</li>
+                    </ul>
+
+                </div>
+                
+                <div className='col'>
+                    <h4>QUICK ACCESS</h4>
+                    <ul>
+                        <li>Home</li>
+                        <li>All destination</li>
+                        <li>Available Packages</li>
+                        <li>Offers</li>
+                    </ul>
+
+                </div>
+                <div className='col'>
+                    <h4>FIND US ON</h4>
+                    <ul>
+                        <li>YouTube</li>
+                        <li>Twitter</li>
+                        <li>Instagram</li>
+                        <li>TikTok</li>
+                        <li>X</li>
+                    </ul>
+
+                </div>
+                <div className='col'>
+                    <p className='col-sm'>
+Copyright@{new Date().getFullYear()}.Kenya Tourism Board. All Rights Reserved. | Tours Kenya
+                    </p>
+
+                </div>
+            </div>
+
+        </div>
+    
+
+    </div>
+  )
+}
+
+export default Footer
