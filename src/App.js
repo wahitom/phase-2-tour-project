0a2ee3d2df2f5ccb983c7ca3b646969a2b8dd656
David <dave.obat@student.moringashool.com>
2023-11-07 22:42:02 +0300

    components exported

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import Availablepackages from "./components/Availablepackages";
+import Destination from "./components/Destination";
 import Footer from "./components/Footer";
+import Home from "./components/Home";
+import NavBar from "./components/NavBar";
+import Offers from "./components/Offers";
 
 function App() {
   return (
     <div >
-
+     <NavBar/>
+     <Home/>
+     <Destination/>
+     <Availablepackages/>
+     <Offers/>
+     <Footer/>
     </div>
   );
 }
---
+++ src/components/Availablepackages.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Availablepackages() {
+  return (
+    <div>Availablepackages</div>
+  )
+}
+
+export default Availablepackages
