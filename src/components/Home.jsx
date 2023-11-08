38bb84f59f5d66495a882da570e4ee15c9793180
David <dave.obat@student.moringashool.com>
2023-11-08 21:31:12 +0300

    home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-
-
+import "./Home.css";
+import { NavLink } from "react-router-dom";
 function Home() {
   return (
-  <div>Home</div>
-  )
+    <div className="homepage">
+     
+      <div className="content">
+        <h1>Welcome To</h1>
+        <NavLink className="Link" to="/destination">
+                  Explore Our Destinations
+                </NavLink>
+      </div>
+    </div>
+ )
 }
 
 export default Home
