13137c00c7b7be9c4e9544f86cc9b4b23cd636c4
MWANZIA <shem.masoo@student.moringaschool.com>
2023-11-08 16:05:58 +0300

    second phase

--- src/components/Destination.jsx
+++ src/components/Destination.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Button } from 'react-bootstrap'
-
+import { Button ,Card, CardText, Container} from 'react-bootstrap'
+import './Destination.css'
 
 const Destination = () => {
   const handleViewPackage = () => {
@@ -8,18 +8,45 @@ const Destination = () => {
   };
  
   return (
-     <div className="beaches">
-       <div className="image">
-         <img src="https://imgs.search.brave.com/-yy_EnFpwxKUx57PC8V5z9jkJotplU9DNnxUhGP8C18/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9hamtl/bnlhc2FmYXJpcy5j/b20vd3AtY29udGVu/dC91cGxvYWRzLzIw/MjMvMDUvYmVzdC1i/ZWFjaGVzLWluLW1v/bWJhc2EtYmcxLTEw/MjR4NjgzLndlYnA" alt="beaches" />
-       </div>
-       <div className="info">
-         <h3>Mombasa North Coast</h3>
-         <p>Diani / Ukunda, Malindi / Watamu, Lamu</p>
-         <Button onClick={handleViewPackage}>View Package</Button>
-       </div>
-     </div>
+    <Container style={{
+      display:'flex',
+      flexDirection:'row'
+    }}>
+        <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src= "https://www.masaimara.com/assets/img/masai-mara.jpg" />
+      <Card.Body>
+        <Card.Title>MaasaiMara</Card.Title>
+        <Card.Text>
+        Experience the untamed beauty of the Maasai Mara, a world-renowned wildlife sanctuary in Kenya. Embark on a thrilling adventure as you witness the iconic Great Migration, where millions of wildebeest and zebras traverse the vast plains in search of greener pastures. Explore the stunning landscapes of rolling savannah, acacia-dotted plains, and meandering rivers teeming with diverse wildlife",
+          
+        </Card.Text>
+<CardText>
+  Prices : $ 264
+</CardText>
+        <Button onClick={handleViewPackage}> View Package</Button>
+      </Card.Body>
+    </Card>
+
+
+
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src= "https://media.cnn.com/api/v1/images/stellar/prod/170720165140-kenya-beaches-diani-camels-on-the-beach-photo-courtesy-afrochic-diani-beach.jpg?q=w_1600,h_900,x_0,y_0,c_fill/h_618" />
+      <Card.Body>
+        <Card.Title>Diani</Card.Title>
+        <Card.Text>
+        "Diani Beach, a tropical paradise along the Kenyan coast, beckons with its powder-white sands, crystal-clear turquoise waters, and swaying palm trees. This idyllic destination offers a perfect blend of relaxation and adventure. Stretching for miles, Diani Beach is a haven for sunseekers, water sports enthusiasts, and nature lovers alike.",
+        </Card.Text>
+        <CardText>
+          prices : $ 364
+        </CardText>
+        <Button onClick={handleViewPackage}> View Package</Button>
+      </Card.Body>
+    </Card>
+    </Container>
   );
- };
+}
+
+
  
 
 export default Destination
