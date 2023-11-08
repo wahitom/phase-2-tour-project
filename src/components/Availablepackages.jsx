d039104494687e6992877ac4b9cc935fa854327b
Miriam Wahito Maina <miriammaina51@gmail.com>
2023-11-08 15:07:09 +0300

    Updated styling in each respective child of AvailabePackages

--- src/components/Availablepackages.jsx
+++ src/components/Availablepackages.jsx
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
+
+import Stack from "react-bootstrap/Stack";
 
 import Amboseli from "./PackagesChild/Amboseli";
 import Diani from "./PackagesChild/Diani";
@@ -9,14 +11,14 @@ import RiftValley from "./PackagesChild/RiftValley";
 
 const Availablepackages = () => {
   return (
-    <>
-      <MaasaiMara />
-      <Diani />
-      <Malindi />
-      <Nakuru />
-      <RiftValley />
-      <Amboseli />
-    </>
+    <Stack gap={3}>
+      <MaasaiMara className="p-2" />
+      <Diani className="p-2" />
+      <Malindi className="p-2" />
+      <Nakuru className="p-2" />
+      <RiftValley className="p-2" />
+      <Amboseli className="p-2" />
+    </Stack>
   );
 };
 
--- src/components/PackagesChild/MaasaiMara.js
+++ src/components/PackagesChild/MaasaiMara.js
@@ -4,6 +4,7 @@ import { Maasaimara } from "./PackageData";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import Stack from "react-bootstrap/Stack";
+import { CardImgOverlay, Col, Row } from "react-bootstrap";
 
 const MaasaiMara = () => {
   const [showPackages, setShowPackages] = useState(false);
@@ -23,24 +24,53 @@ const MaasaiMara = () => {
 
   return (
     <>
-      <Card style={{ width: "18rem" }}>
-        <Card.Img variant="top" src="holder.js/100px180" />
-        <Card.Body>
-          <Card.Title>{destination.destination}</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-          <Button variant="primary" onClick={togglePackages}>
-            {showPackages ? "Hide Packages" : "See More packages"}
-          </Button>
-        </Card.Body>
-      </Card>
+      <Row xs={1} md={2} className="g-4">
+        <Col>
+          <Card style={{ width: "150vh", height: "40vh" }}>
+            <div style={{ display: "flex" }}>
+              <div style={{ flex: 1 }}>
+                <Card.Img
+                  variant="top"
+                  src={destination.destinationImage}
+                  style={{ width: "70vh", height: "40vh" }}
+                />
+                {/* <CardImgOverlay>
+                  <h1>{destination.destination}</h1>
+                </CardImgOverlay> */}
+              </div>
+              <div
+                style={{
+                  flex: 0.5,
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "end",
+                }}
+              >
+                <Card.Body>
+                  <Card.Title>{destination.destination}</Card.Title>
+                  <Card.Text>
+                    This is a longer card with supporting text below as a
+                    natural lead-in to additional content. This content is a
+                    little bit longer.
+                  </Card.Text>
+                  <Button variant="primary" onClick={togglePackages}>
+                    {showPackages ? "Hide Packages" : "See More packages"}
+                  </Button>
+                </Card.Body>
+              </div>
+            </div>
+          </Card>
+        </Col>
+      </Row>
 
       <Stack direction="horizontal" gap={3}>
         {showPackages && (
-          <Card style={{ width: "18rem" }} className="p-2">
-            <Card.Img variant="top" src="holder.js/100px180" />
+          <Card style={{ width: "16rem", marginTop: "30px" }} className="p-2">
+            <Card.Img
+              variant="top"
+              src={destination.threeDayerImg}
+              style={{ height: "30vh" }}
+            />
             <Card.Body>
               <Card.Title>{destination.threeDayer}</Card.Title>
               <Card.Text>
@@ -53,8 +83,12 @@ const MaasaiMara = () => {
         )}
 
         {showPackages && (
-          <Card style={{ width: "18rem" }} className="p-2">
-            <Card.Img variant="top" src="holder.js/100px180" />
+          <Card style={{ width: "16rem", marginTop: "30px" }} className="p-2">
+            <Card.Img
+              variant="top"
+              src={destination.fourDayerImg}
+              style={{ height: "30vh" }}
+            />
             <Card.Body>
               <Card.Title>{destination.fourDayer}</Card.Title>
               <Card.Text>
@@ -67,10 +101,32 @@ const MaasaiMara = () => {
         )}
 
         {showPackages && (
-          <Card style={{ width: "18rem" }} className="p-2">
-            <Card.Img variant="top" src="holder.js/100px180" />
+          <Card style={{ width: "16rem", marginTop: "30px" }} className="p-2">
+            <Card.Img
+              variant="top"
+              src={destination.fiveDayerImg}
+              style={{ height: "30vh" }}
+            />
             <Card.Body>
               <Card.Title>{destination.fiveDayer}</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </Card.Text>
+              <Button variant="primary">See More packages</Button>
+            </Card.Body>
+          </Card>
+        )}
+
+        {showPackages && (
+          <Card style={{ width: "16rem", marginTop: "30px" }} className="p-2">
+            <Card.Img
+              variant="top"
+              src={destination.weekendImg}
+              style={{ height: "30vh" }}
+            />
+            <Card.Body>
+              <Card.Title>{destination.weekend}</Card.Title>
               <Card.Text>
                 Some quick example text to build on the card title and make up
                 the bulk of the card's content.
