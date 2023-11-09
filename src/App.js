e52a68c1d448608405c67e65b0a5ae0ccb2115ba
Maganga Mwambonu <codebymaganga@gmail.com>
2023-11-09 12:31:58 +0300

    Search Bar displays on All pages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import { Route, Routes } from "react-router";
 import NavBar from "./components/NavBar";
-
+import Availablepackages from "./components/Availablepackages";
 import Offers from "./components/Offers";
 import Destination from "./components/Destination";
 import Home from "./components/Home";
 import Footer from "./components/Footer";
 import Userform from "./components/Userform";
 import "./App.css";
+import SearchBar from "./components/SearchBar";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <SearchBar />
 
       <Routes>
         <Route path="/" element={<Home />} />
