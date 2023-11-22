6cbae258f67d048d9774d20752d60de21f20435c
David <dave.obat@student.moringashool.com>
2023-11-22 12:23:51 +0300

    changes

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
