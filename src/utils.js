5979d1bc24a99ec68c648b9daaea52b9ee1572ea
Miriam Wahito Maina <miriammaina51@gmail.com>
2024-03-19 20:56:56 +0300

    changed base url

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,8 @@
-export const BASE_URL =
+/* export const BASE_URL =
   process.env.NODE_ENV === "development"
     ? `https://my-json-server.typicode.com/wahitom/phase-2-tour-project/travels`
     : process.env.BASE_URL;
+ */
+
+export const BASE_URL =
+  "https://my-json-server.typicode.com/wahitom/phase-2-tour-project/travels";
